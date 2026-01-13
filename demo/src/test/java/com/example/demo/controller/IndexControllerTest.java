0d74927843613f91ea4e84aaf074c50bb4057fd2
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2026-01-12 21:12:28 -0500

    clase 12/01/26

--- demo/src/test/java/com/example/demo/controller/IndexControllerTest.java
+++ demo/src/test/java/com/example/demo/controller/IndexControllerTest.java
@@ -1,16 +1,35 @@
 package com.example.demo.controller;
-
+ 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
-
+ 
+import com.example.demo.controller.IndexController;
+ 
 public class IndexControllerTest {
+ 
     @Test
-    public void testWelcome() {
-        IndexController controller = new IndexController();
-        String[] input = {"hello", null, "world"};
-        String expected = "param[0]Hello\nparam[1]null\nparam[2]world\n";
-        String result = controller.welcome(input);
-        assertEquals(expected, result);        
+    public void welcomeTest(){
+ 
+        IndexController indexController = new IndexController();
+        String[] paramArray = null;
+        String resultadoActual = "El array esta vacio";
+        String resultadoReal = indexController.welcome(paramArray);
+        assertEquals(resultadoActual,resultadoReal);
+    }
+    @Test
+    public void welcomeItemNull(){
+        IndexController indexController = new IndexController();
+        String[] paramArray = new String[3];
+        String resultadoActual = "param[0]null\n" + "param[1]null\n" + "param[2]null\n";
+        String resultadoReal = indexController.welcome(paramArray);
+        assertEquals(resultadoActual, resultadoReal);
+    }
+    @Test
+    public void welcomeItemNotNull(){
+        IndexController indexController = new IndexController();
+        String[] paramArray = new String[]{"Practica", "con", "JUnit"};
+        String resultadoActual = "param[0]Practica\n" + "param[1]con\n" + "param[2]JUnit\n";
+        String resultadoReal = indexController.welcome(paramArray);
+        assertEquals(resultadoActual, resultadoReal);
     }
 }
